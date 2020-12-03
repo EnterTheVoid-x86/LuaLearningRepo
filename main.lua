44847ee3876cd2f1e74da264e2f543998bc70230
EnterTheVoid <64978924+EnterTheVoid-x86@users.noreply.github.com>
2020-12-03 18:35:52 -0500

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,35 @@
+line=600
+--This variable is what we want to be set to 100 or 200. You may change this.--
+if line == 100 or line == 200
+ then
+  print("It worked!")
+ elseif line == 69
+  then
+  	while ( true )
+  	do
+  	    print ("nice")
+  	end
+  elseif line == 420
+      then
+      print ("Weed number")
+      elseif line == 0
+          then
+              print ("Dang thats pretty low")
+              else
+      print ("It worked! But the variable isnt what we want it to be. (100 or 200")
+      if line >= 200
+    then
+    print ("Wanna get stuck in a loop due to your terrible mistakes? I mean, who the heck manages to just randomly choose a number higher than 200? (yes/no)")
+    local thing = io.read()
+    if thing == 'yes'
+    then
+        print ("Ok")
+        while ( true )
+        do
+            print ("weeeeeeeeee")
+            end
+            else
+               print ("Fine.")
+end  	
+end
+end
